e5c03d086d6cb52ff3c9e5b28978c173ad1db5fc
periyana <113885495+periyana@users.noreply.github.com>
2024-10-17 01:07:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://github.com/periyana/portofolio/',
   server: {
     hmr: {
       overlay: false, // Disable the HMR error overlay
